c3712351c69cfbacfe8ae5e5e1335ff82627c872
YashRaj8190 <yashraj292004@gmail.com>
2023-10-18 12:49:05 +0530

    made functions for start and reset

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ var resetBtnrObj = document.querySelector("#Reset-btn")
 var isTimerRunning =false
 var interval
 resetBtnrObj.addEventListener("click",()=>{
-    console.log("reset button clicked");
-    timerObj.textContent="00:00";
-    startBtnObj.textContent ="Start";
-    isTimerRunning = false
-    clearInterval(interval)
+    resetTimer();
 })
  startBtnObj.addEventListener("click",()=>{
+    startStopTimer();
+})
+
+document.addEventListener("keydown",function (event){
+    if(event.key=="s"){
+        startStopTimer();
+    }
+    else if(event.key=="r"){
+        resetTimer();
+    }
+});
+
+function startStopTimer(){
     console.log("start button clicked");
     if(isTimerRunning){
         startBtnObj.textContent ="Start";
@@ -24,7 +33,7 @@ resetBtnrObj.addEventListener("click",()=>{
             var timerText = timerObj.textContent
             var timerArr = timerText.split(":") 
             var minutes = Number(timerArr[0])
-            var seconds = Number(timerArr[1])  
+            var seconds = Number(timerArr[1]) 
                 seconds++ 
             if(seconds == 60){
                 minutes++
@@ -39,46 +48,14 @@ resetBtnrObj.addEventListener("click",()=>{
             timerObj.textContent = minutes + ":" + seconds ;
         }, 1000)
     }
-})
+}
 
-document.addEventListener("keydown",function (event){
-    if(event.key=="s"){
-        console.log("start button clicked");
-    if(isTimerRunning){
-        startBtnObj.textContent ="Start";
-        isTimerRunning = false
-        clearInterval(interval)
-    }
-    else{
-        startBtnObj.textContent="Stop";
-        isTimerRunning = true
-        interval = setInterval(function(){
-            var timerText = timerObj.textContent
-            var timerArr = timerText.split(":") 
-            var minutes = Number(timerArr[0])
-            var seconds = Number(timerArr[1])  
-                seconds++ 
-            if(seconds == 60){
-                minutes++
-                seconds = 0
-            }
-            if(minutes < 10){
-                minutes = "0" + minutes
-            }
-            if(seconds < 10){
-                seconds = "0" + seconds
-            }
-            timerObj.textContent = minutes + ":" + seconds ;
-        }, 1000)
-    }
-    }
-    else if(event.key=="r"){
-        console.log("reset button clicked");
-        timerObj.textContent="00:00";
-        startBtnObj.textContent ="Start";
-        isTimerRunning = false
-        clearInterval(interval)
-    }
-});
+function resetTimer(){
+    console.log("reset button clicked");
+    timerObj.textContent="00:00";
+    startBtnObj.textContent ="Start";
+    isTimerRunning = false
+    clearInterval(interval)
+}
  
  
